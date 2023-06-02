d5a97fdfcb47b0e1310ae3a767213ea8b6027865
sir-braz <vinicius@pop-os.localdomain>
2023-06-02 10:02:16 -0700

    Created the Pothole model with the required fields: latitude, longitude, description, and createdAt

--- backend/model/pothole.js
+++ backend/model/pothole.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose')
 
 const pothoSchema = new mongoose.Schema({
-
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longetitude: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
 })
 
 const Pothole = mongoose.model('Pothole', pothoSchema)
