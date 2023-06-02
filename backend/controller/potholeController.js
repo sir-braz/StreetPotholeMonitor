d7107514a2d256ead3737b2c1ee6abdd50d7bc9f
sir-braz <vinicius@pop-os.localdomain>
2023-06-02 11:07:24 -0700

    Added the logic to handle the reporting of a new pothole. The function receives latitude, longitude, and description from the request body, creates a new Pothole object, saves it to the database, and returns a success message. Proper error handling is also implemented to log errors and send an appropriate response in case of any errors during the reporting process

--- backend/controller/potholeController.js
+++ backend/controller/potholeController.js
@@ -3,7 +3,16 @@ const Pothole = require('../model/pothole')
 //Route to report a new pothole
 exports.reportPothole = async (req, res, next) => {
     try{
-
+        const {latitude, longitude, description} = req.body
+        // Create a new Pothole object
+        const pothole = new Pothole({
+            latitude,
+            longitude,
+            description
+        })
+        //Save the pothole to the database
+        const savedPothole = await pothole.save()
+        res.status(200).json({error: 'OK!'})
     }catch(err){
         console.log(err)
         res.status(500).json({'message': 'Error reporting'})
